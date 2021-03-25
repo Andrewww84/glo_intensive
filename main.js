4eb00b7140e95d53b51cc336fe60c286b9d1de7b
Andrewww84 <72757873+Andrewww84@users.noreply.github.com>
2021-03-25 18:11:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -14,15 +14,148 @@ const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.getElementById('modal-cart');
 const modalClose = document.querySelector('.modal-close');
 
-const openModal = function () {
-  modalCart.classList.add('show');
+// goods
+
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
+const longGoodsList = document.querySelector('.long-goods-list');
+const showClothing = document.querySelectorAll('.show-clothing');
+const showAccessories = document.querySelectorAll('.show-accessories');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cartTableTotal = document.querySelector('.card-table__total');
+
+// server request & response
+ 
+const getGoods = async () => {
+  const result = await fetch('db/db.json');
+  console.log(result);
+  if (!result.ok) {
+    throw 'Ошибочка вышла; ' + result.status;
+  }
+  return result.json();
 };
 
-const closeModal = function () {
+const cart = {
+  cartGoods: [
+    
+  ],
+  renderCart(){
+    cartTableGoods.textContent = '';
+    this.cartGoods.forEach(({ id, name, price, count }) => {
+      const trGood = document.createElement('tr');
+      trGood.className = 'cart-item';
+      trGood.dataset.id = id;
+      trGood.innerHTML = `
+          <td>${name}</td>
+					<td>${price}$</td>
+					<td><button class="cart-btn-minus">-</button></td>
+					<td>${count}</td>
+					<td><button class="cart-btn-plus">+</button></td>
+					<td>${price * count}$</td>
+					<td><button class="cart-btn-delete">x</button></td>
+      `;
+      cartTableGoods.append(trGood);
+    });
+
+    const totalPrice = this.cartGoods.reduce((sum, item) => {
+      return sum + (item.price * item.count);
+    }, 0);
+    
+    cartTableTotal.textContent = totalPrice + '$';
+
+  },
+  deleteGood(id){
+    this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+    this.renderCart();
+  },
+  minusGood(id){
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+        if (item.count <= 1) {
+          this.deleteGood(id)
+        } else {
+          item.count--;
+        }
+        break;
+      }
+    }
+    this.renderCart();
+  },
+  
+  plusGood(id){
+    for (const item of this.cartGoods) {
+      if (item.id === id) {
+          item.count++;
+          break;
+      }
+    }
+    this.renderCart();
+  },
+  addCartGoods(id){
+    const goodItem = this.cartGoods.find(item => item.id === id);
+    if (goodItem) {
+      this.plusGood(id);
+    } else {
+      getGoods()
+        .then(data => data.find(item => item.id === id))
+        .then(({ id, name, price }) => {
+          this.cartGoods.push({
+            id,
+            name,
+            price,
+            count: 1
+          });
+        });
+    }
+  },
+}
+
+
+document.body.addEventListener('click', event => {
+  const addToCart = event.target.closest('.add-to-cart');
+  console.log(addToCart);
+
+  if (addToCart){
+    cart.addCartGoods(addToCart.dataset.id)
+  }
+});
+
+
+
+
+
+cartTableGoods.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName === "BUTTON") {
+    const id = target.closest('.cart-item').dataset.id;
+
+    if (target.classList.contains('cart-btn-delete')) {
+      cart.deleteGood(id);
+    };
+
+    if (target.classList.contains('cart-btn-minus')) {
+      cart.minusGood(id);
+    }
+
+    if (target.classList.contains('cart-btn-plus')) {
+      cart.plusGood(id);
+    }
+  }
+  
+})
+
+const openModal = () => {
+  modalCart.classList.add('show');
+  cart.renderCart();
+};
+
+const closeModal = () => {
   modalCart.classList.remove('show');
 };
 
 buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
 
 // close modal by overlay and button 'X'
 
@@ -40,7 +173,7 @@ modalCart.addEventListener('click', (event) => {
   const scrollLinks = document.querySelectorAll('a.scroll-link');
 
   for (const scrollLink of scrollLinks) {
-    scrollLink.addEventListener('click', function (event) {
+    scrollLink.addEventListener('click', event => {
       event.preventDefault();
       const id = scrollLink.getAttribute('href');
       document.querySelector(id).scrollIntoView({
@@ -52,22 +185,6 @@ modalCart.addEventListener('click', (event) => {
 })
 
 
-// goods
-
-const more = document.querySelector('.more');
-const navigationLink = document.querySelectorAll('.navigation-link');
-const longGoodsList = document.querySelector('.long-goods-list');
-
-// server request & response
-
-const getGoods = async function () {
-  const result = await fetch('db/db.json');
-  console.log(result);
-  if (!result.ok) {
-    throw 'Ошибочка вышла; ' + result.status;
-  }
-  return result.json();
-};
 
 
 const createCard = function ({ label, name, img, description, price, id }) {
@@ -100,29 +217,42 @@ const renderCards = function (data) {
   document.body.classList.add('show-goods');
 };
 
-more.addEventListener('click', function (event) {
+const showAll = event => {
   event.preventDefault();
   getGoods().then(renderCards);
-});
+}
 
+viewAll.forEach(function(elem) {
+  elem.addEventListener('click', showAll);
+});
 
 
 const filterCards = function (field, value) {
   getGoods()
-    .then(function (data) {
-      const filteredGoods = data.filter(function (good) {
-        return good[field] === value;
-      });
-      return filteredGoods;
-    })
+    .then(data => data.filter(good => good[field] === value))
     .then(renderCards);
 };
 
 navigationLink.forEach(function (link) {
-  link.addEventListener('click', function(event) {
+  link.addEventListener('click', event => {
     event.preventDefault();
     const field = link.dataset.field;
     const value = link.textContent;
     filterCards(field, value);
   })
+});
+
+
+showAccessories.forEach(item => {
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    filterCards('category', 'Accessories');
+  });
+});
+
+showClothing.forEach(item => {
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    filterCards('category', 'Clothing');
+  });
 });
